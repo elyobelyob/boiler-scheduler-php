acf9907ded136934192ab3f360660ea8bc1c46fb
Nick Boyle <nick@elyob.com>
2013-03-06 21:43:56 +0000

    Latest SQL file

--- sql/boiler.sql
+++ sql/boiler.sql
@@ -1,3 +1,16 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : 192.168.1.87
+ Source Server Version : 50528
+ Source Host           : localhost
+ Source Database       : boiler
+
+ Target Server Version : 50528
+ File Encoding         : utf-8
+
+ Date: 03/06/2013 21:42:42 PM
+*/
 
 SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
@@ -9,15 +22,14 @@ DROP TABLE IF EXISTS `configuration`;
 CREATE TABLE `configuration` (
   `key` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `value` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `type` enum('int','string','boolean','long') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'string',
-  PRIMARY KEY (`key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `type` enum('int','string','boolean','long') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'string'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 --  Records of `configuration`
 -- ----------------------------
 BEGIN;
-INSERT INTO `configuration` VALUES ('boostTime', '60', 'int'), ('holidayFrom', '1361404800000', 'long'), ('holidayTemp', '8', 'int'), ('holidayUntil', '1362009600000', 'long');
+INSERT INTO `configuration` VALUES ('boostTime', '60', 'int'), ('holidayFrom', '1362268800', 'long'), ('holidayTemp', '8', 'int'), ('holidayTo', '1362349140', 'long');
 COMMIT;
 
 -- ----------------------------
@@ -25,21 +37,15 @@ COMMIT;
 -- ----------------------------
 DROP TABLE IF EXISTS `override`;
 CREATE TABLE `override` (
-  `id` int(5) NOT NULL,
+  `id` int(5) NOT NULL AUTO_INCREMENT,
   `type` varchar(75) DEFAULT NULL,
   `date` datetime DEFAULT NULL,
   `length` int(5) DEFAULT NULL,
   `boost` int(1) DEFAULT '1',
   `enabled` int(1) DEFAULT '1',
+  `heatingTemp` int(2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
---  Records of `override`
--- ----------------------------
-BEGIN;
-INSERT INTO `override` VALUES ('1', 'heat', '2013-02-13 14:20:01', '120', '1', '1');
-COMMIT;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 --  Table structure for `schedule`
@@ -60,13 +66,13 @@ CREATE TABLE `schedule` (
   KEY `day` (`day`),
   KEY `hourOn` (`timeOn`),
   CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`group`) REFERENCES `schedule_groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=98 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=98 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 --  Records of `schedule`
 -- ----------------------------
 BEGIN;
-INSERT INTO `schedule` VALUES ('58', '1', '2', '05:00:00', '07:00:00', '1', '18', '0', '0'), ('59', '1', '3', '05:00:00', '07:00:00', '1', '18', '0', '0'), ('60', '1', '4', '05:00:00', '07:00:00', '1', '18', '0', '0'), ('61', '1', '5', '05:00:00', '07:00:00', '1', '18', '0', '0'), ('62', '1', '6', '05:00:00', '07:00:00', '1', '18', '0', '0'), ('63', '3', '1', '19:00:00', '23:00:00', '1', '18', '0', '0'), ('64', '1', '4', '19:00:00', '23:00:00', '1', '18', '0', '0'), ('65', '1', '2', '19:00:00', '23:00:00', '1', '18', '0', '0'), ('66', '1', '3', '19:00:00', '23:00:00', '1', '18', '0', '0'), ('67', '3', '7', '07:00:00', '10:00:00', '1', '18', '0', '0'), ('68', '3', '7', '19:00:00', '23:00:00', '1', '18', '0', '0'), ('69', '1', '5', '19:00:00', '23:00:00', '1', '18', '0', '0'), ('70', '1', '6', '19:00:00', '23:00:00', '1', '18', '0', '0'), ('71', '3', '1', '07:00:00', '10:00:00', '1', '18', '0', '0'), ('97', '1', '3', '05:00:00', '07:00:00', '1', '18', '0', '0');
+INSERT INTO `schedule` VALUES ('58', '1', '2', '05:00:00', '07:00:00', '1', '18', '0', '1'), ('60', '1', '4', '05:00:00', '07:00:00', '1', '18', '0', '1'), ('61', '1', '5', '05:00:00', '07:00:00', '1', '18', '0', '1'), ('62', '1', '6', '05:00:00', '07:00:00', '1', '18', '0', '1'), ('63', '3', '1', '19:00:00', '23:00:00', '1', '18', '0', '1'), ('64', '1', '4', '19:00:00', '23:00:00', '1', '18', '0', '1'), ('65', '1', '2', '19:00:00', '23:00:00', '1', '18', '0', '1'), ('66', '1', '3', '19:00:00', '23:00:00', '1', '18', '0', '1'), ('67', '3', '7', '07:00:00', '10:00:00', '1', '18', '0', '1'), ('68', '3', '7', '19:00:00', '23:00:00', '1', '18', '0', '1'), ('69', '1', '5', '19:00:00', '23:00:00', '1', '18', '0', '1'), ('70', '1', '6', '19:00:00', '23:00:00', '1', '18', '0', '1'), ('71', '3', '1', '07:00:00', '10:00:00', '1', '18', '0', '1'), ('97', '1', '3', '05:00:00', '07:00:00', '1', '18', '0', '1');
 COMMIT;
 
 -- ----------------------------
@@ -78,7 +84,7 @@ CREATE TABLE `schedule_groups` (
   `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 --  Records of `schedule_groups`
