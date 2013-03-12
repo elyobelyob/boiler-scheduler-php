736d368389fc6c2180e6123efbdf4690817bc039
Nick Boyle <nick@elyob.com>
2013-03-12 10:03:07 +0000

    Move scheduler log and pagerefresh

--- application/modules/elyob/controllers/StatusController.php
+++ application/modules/elyob/controllers/StatusController.php
@@ -4,6 +4,7 @@ class Elyob_StatusController extends Zend_Controller_Action
 { 
     public function indexAction()
     {    	
+    	$this->view->headScript()->appendFile('/js/pagerefresh.js');
     	$this->view->headScript()->appendFile('/js/clock.js');
     	$this->view->headTitle('Status');
     }
---
+++ public/js/pagerefresh.js
@@ -0,0 +1,6 @@
+function reFresh() {
+  location.reload(true)
+}
+/* Set the number below to the amount of delay, in milliseconds,
+you want between page reloads: 1 minute = 60000 milliseconds. */
+window.setInterval("reFresh()",60000);
