c9b2b8887cb579e7a7cc97c7924741060e95e285
Nick Boyle <nick@elyob.com>
2013-03-12 10:30:26 +0000

    reduce js refresh

--- public/js/pagerefresh.js
+++ public/js/pagerefresh.js
@@ -3,4 +3,4 @@ function reFresh() {
 }
 /* Set the number below to the amount of delay, in milliseconds,
 you want between page reloads: 1 minute = 60000 milliseconds. */
-window.setInterval("reFresh()",60000);
+window.setInterval("reFresh()",57000);
