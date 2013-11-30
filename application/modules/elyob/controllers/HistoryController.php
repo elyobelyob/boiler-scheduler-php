0a03e90a9186b97042be4a71f8ec456214a545bd
Nick Boyle <nick@elyob.com>
2013-11-30 16:44:12 +0000

    remove refresh on history

--- application/modules/elyob/controllers/HistoryController.php
+++ application/modules/elyob/controllers/HistoryController.php
@@ -4,8 +4,6 @@ class Elyob_HistoryController extends Zend_Controller_Action
 { 
     public function indexAction()
     {    	
-    	$this->view->headScript()->appendFile('/js/pagerefresh.js');
-    	$this->view->headScript()->appendFile('/js/clock.js');
     	$this->view->headTitle('History');
     }
 }
